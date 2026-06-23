e0b54fb116ea9e880797a3e10a98765273cb7fdb
Chuck Smith <eclectic-coding@users.noreply.github.com>
2026-06-23 07:50:46 -0400

    Add Capybara class and outlet matchers

--- lib/stimulus_spec/capybara/matchers.rb
+++ lib/stimulus_spec/capybara/matchers.rb
@@ -4,6 +4,8 @@ require_relative "matchers/have_stimulus_controller"
 require_relative "matchers/have_stimulus_action"
 require_relative "matchers/have_stimulus_target"
 require_relative "matchers/have_stimulus_value"
+require_relative "matchers/have_stimulus_class"
+require_relative "matchers/have_stimulus_outlet"
 
 module StimulusSpec
   module Capybara
---
+++ lib/stimulus_spec/capybara/matchers/have_stimulus_class.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module StimulusSpec
+  module Capybara
+    module Matchers
+      class HaveStimulusClass
+        def initialize(controller, name, expected = nil)
+          @controller = controller.to_s
+          @name = name.to_s
+          @expected = expected
+          @attr = "data-#{@controller}-#{@name}-class"
+        end
+
+        def matches?(page)
+          @page = page
+          element = page.first("[#{@attr}]", minimum: 0, wait: 0)
+          return false unless element
+
+          if @expected
+            @actual = element[@attr]
+            @actual == @expected.to_s
+          else
+            true
+          end
+        end
+
+        def does_not_match?(page)
+          !matches?(page)
+        end
+
+        def failure_message
+          if @expected && @actual
+            "expected #{@attr} to be \"#{@expected}\" but was \"#{@actual}\""
+          else
+            "expected to find an element with #{@attr} on the page"
+          end
+        end
+
+        def failure_message_when_negated
+          "expected not to find an element with #{@attr} on the page"
+        end
+
+        def description
+          if @expected
+            "have Stimulus class \"#{@name}\" with \"#{@expected}\" for controller \"#{@controller}\""
+          else
+            "have Stimulus class \"#{@name}\" for controller \"#{@controller}\""
+          end
+        end
+      end
+
+      def have_stimulus_class(controller, name, expected = nil)
+        HaveStimulusClass.new(controller, name, expected)
+      end
+    end
+  end
+end
